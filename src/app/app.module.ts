ef5cdbb3948a2b8ab5e3fba6b7f2383c8f0c594e
Pablo Riego <pabloriego21@gmail.com>
2022-02-16 20:59:33 -0300

    inicio componentes separacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaEducacionComponent } from './componentes/experiencia-educacion/experiencia-educacion.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    ExperienciaEducacionComponent,
   ],
   imports: [
     BrowserModule,
